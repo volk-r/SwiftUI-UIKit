febf0414f461e61b61c58c25fecd1cc465bde5b1
Roman Romanov <rromanov.vk@gmail.com>
2024-11-20 23:48:54 +0500

    Initial Commit

---
+++ SwiftUI-UIKit/SwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKitApp.swift
+//  SwiftUI-UIKit
+//
+//  Created by Roman Romanov on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
